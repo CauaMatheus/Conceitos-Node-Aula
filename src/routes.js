6f86b454bee44a784d5ebece3cf1959597e4aefd
CauaMatheus <caua10000@gmail.com>
2021-03-08 09:30:39 -0300

    HTTP Methods

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
-const { Router } = require("express");
+const { Router, response, request } = require("express");
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
   return response.json({
-    "message": "Hello World!"
+    "message": "Hello World! - Fundamentos NodeJs"
   })
+});
+
+routes.get('/courses', (request, response) => {
+  return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+});
+
+routes.put('/courses', (request, response) => {
+  return response.json(["Curso6", "Curso2", "Curso3", "Curso4"]);
+});
+
+routes.patch('/', (request, response) => {
+  return response.json(["Curso6", "Curso7", "Curso3", "Curso4"]);
+})
+
+routes.delete('/', (request, response) => {
+  return response.json(["Curso6", "Curso7", "Curso4"]);
 });
 
 module.exports = routes;
