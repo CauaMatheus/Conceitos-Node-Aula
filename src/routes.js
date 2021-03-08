cd2a876c4f1decc5c85cd917524a1e683a9938cf
CauaMatheus <caua10000@gmail.com>
2021-03-08 10:02:14 -0300

    Params Types

--- src/routes.js
+++ src/routes.js
@@ -2,25 +2,31 @@ const { Router, response, request } = require("express");
 
 const routes = Router()
 
-routes.get('/', (request, response) => {
+routes.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json({
     "message": "Hello World! - Fundamentos NodeJs"
   })
 });
 
-routes.get('/courses', (request, response) => {
+routes.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
 });
 
-routes.put('/courses', (request, response) => {
+routes.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(["Curso6", "Curso2", "Curso3", "Curso4"]);
 });
 
-routes.patch('/', (request, response) => {
+routes.patch('/courses/:id', (request, response) => {
   return response.json(["Curso6", "Curso7", "Curso3", "Curso4"]);
 })
 
-routes.delete('/', (request, response) => {
+routes.delete('/courses/:id', (request, response) => {
   return response.json(["Curso6", "Curso7", "Curso4"]);
 });
 
